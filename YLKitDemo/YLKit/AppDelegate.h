64dd1a11448ef4eebcf99a0565992deaa46d70c8
zhouyouyali <814307045@qq.com>
2017-04-26 16:17:38 +0800

    first create pods

---
+++ YLKitDemo/YLKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLKit
+//
+//  Created by zhouyouyali on 2017/4/26.
+//  Copyright © 2017年 Yaali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLKitDemo/YLKit/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  YLKit
+//
+//  Created by zhouyouyali on 2017/4/26.
+//  Copyright © 2017年 Yaali. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BlockType.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *v1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 20, 40)];
+    [v1 setBackgroundColor:[UIColor blueColor]];
+ 
+    block_void v;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
